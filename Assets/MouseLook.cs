848d0654c3299ba6b91ecfedfd929c0b6df3c250
MercJones <jackdunk@hotmail.com>
2021-03-30 16:04:00 -0600

    hooked up player movement and camera control

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:28a98a946c45f07981472ef35d8f15909f993a37927b9827744f72141cbafd63
+size 845
---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42b802d374b0d00132b91b344ab4a4be108a1bddc4d4bd4e0b21d3fbfea41fd8
+size 618
