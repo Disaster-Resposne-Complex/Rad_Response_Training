28923e589e9da30a9e67050e8eb98e90be7ebf2e
MercJones <jackdunk@hotmail.com>
2021-05-09 15:49:38 -0600

    converted dose checker to read in mrem

--- Assets/PlayerDoseScript.cs
+++ Assets/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3952a07c64b2b099499365a572df636391d9084673ba5827b282f21c01ba4fa1
-size 528
+oid sha256:aeec008c07f9353dfe4606ef224d7656de05bba487f0dcddd25807943e37c31f
+size 726
--- Assets/RadRayCast.cs
+++ Assets/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e29f6c42dd180c736cf97ebdb8098fe273e5fb79ed320b65224fde1b2d577e5
-size 1374
+oid sha256:314e76de74245ca2cac2eeacf7e826d7d9345d50985f911c3aa955b5f8f92433
+size 1391
