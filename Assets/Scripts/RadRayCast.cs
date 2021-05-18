665cf669e8c6d445ed82a11cf948deef22b10e56
MercJones <jackdunk@hotmail.com>
2021-05-17 18:30:45 -0600

    basic shielding calculation added

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:314e76de74245ca2cac2eeacf7e826d7d9345d50985f911c3aa955b5f8f92433
-size 1391
+oid sha256:48ac097dab9119b158af69386fe46026d61a6bfe6dde27f43d90369951ad7ff9
+size 1584
