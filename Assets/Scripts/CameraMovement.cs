6711195e0e34b35a9ee300c5b67cf9deeb90b3ca
MercJones <jackdunk@hotmail.com>
2021-05-21 14:23:54 -0600

    Upgraded engine to 2020.3.7f1

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,35 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-
-public class CameraMovement : MonoBehaviour
-{
-    public GameObject player;
-    private Vector3 offset;
-    //This variable I'm adding to represent to disance between the camera and the ball. I intend to rotate the camera with f and j keys or something
-    private Vector3 fallingOffset;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-        fallingOffset.y = player.transform.position.y;
-    }
-
-    
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        if (player.transform.position.y <= -10f)
-        {
-            transform.position = player.transform.position + offset - fallingOffset;
-        }
-        else
-        {
-            transform.position = player.transform.position + offset;
-        }
-        
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c76a37c2b13777af4186ecd3a93227059492ad292c975405ff50616d71576973
+size 951
--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,27 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class InputController : MonoBehaviour
-{
-    static public bool arrowKeySetting;
-    static public float maximumSpeed;
-    public Toggle ArrowsOrMouse;
-    //public float minSlider;
-    //public float maxSlider;
-
-    public Slider SpeedSlider;
-
-    //These function will be referenced in the settings toggle "On Value Changed (Boolean)"
-    public void ArrowControlSetting()
-    {
-        arrowKeySetting = ArrowsOrMouse; //gets true or false from whether box is checked and assigns it to the arrowKeySetting variable
-    }
-
-
-    public void UserSetMaxSpeed()
-    {
-        maximumSpeed = SpeedSlider.value;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7077997952babc5756f426cf47710a4d000b2bef773e4144c35bd5fff2317765
+size 727
