a636caef6c90f7ac55bfd193cadb2980407d3d8b
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-05-24 16:04:08 -0600

    Mouse Sensitivity and Cursor Lock
    Ahhhh, much better

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ac097dab9119b158af69386fe46026d61a6bfe6dde27f43d90369951ad7ff9
-size 1584
+oid sha256:3de07333d5eac5426555b3ff8c3a60666aafbf19e1475e66abc12489d193d377
+size 1586
