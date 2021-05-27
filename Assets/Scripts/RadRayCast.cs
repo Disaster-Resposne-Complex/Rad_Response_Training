02fd9ba614616fd57c71d587460e60fd8777f4a9
MercJones <jackdunk@hotmail.com>
2021-05-26 21:59:20 -0600

    updated to possibly account for alpha, beta, gamma

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ac097dab9119b158af69386fe46026d61a6bfe6dde27f43d90369951ad7ff9
-size 1584
+oid sha256:b717659dc72ec4d5b30309c11ae25a8fec37df00db65e733c002221ede4a6891
+size 1613
