c7b91241b4b020c9425eed54024140217c54261d
MercJones <jackdunk@hotmail.com>
2021-05-26 22:19:25 -0600

    Turned radiation type to an enumeration

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ac097dab9119b158af69386fe46026d61a6bfe6dde27f43d90369951ad7ff9
-size 1584
+oid sha256:cfdcc0aa112f8e339e170b93d819451c5ebffe5d91a4ace43cdcf7483bdbc35b
+size 1699
