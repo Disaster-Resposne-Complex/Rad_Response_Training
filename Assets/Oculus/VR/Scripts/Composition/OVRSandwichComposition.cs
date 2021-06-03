d9526afec6f411c5b8a223f1b6f580d6c835cc48
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-06-03 14:06:09 -0600

    Oculus stuff
    These changes just showed up since the last time I worked on it. I didn't make them, so I'm not sure where they came from. I couldn't discard them either, and I can't make new changes without committing these, so sorry if something breaks.

--- Assets/Oculus/VR/Scripts/Composition/OVRSandwichComposition.cs
+++ Assets/Oculus/VR/Scripts/Composition/OVRSandwichComposition.cs
@@ -1,4 +1,3 @@
-public class OVRSandwichComposition
-{
-	// deprecated since SDK 1.41
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8454fc7fdb8f6b08c370fdc3f0a6fee7a8914431b1b405e56579d11abf6b0db
+size 70
--- Assets/Oculus/VR/Scripts/OVROnCompleteListener.cs
+++ Assets/Oculus/VR/Scripts/OVROnCompleteListener.cs
@@ -1,29 +1,3 @@
-/************************************************************************************
-Copyright : Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
-
-Your use of this SDK or tool is subject to the Oculus SDK License Agreement, available at
-https://developer.oculus.com/licenses/oculussdk/
-
-Unless required by applicable law or agreed to in writing, the Utilities SDK distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-ANY KIND, either express or implied. See the License for the specific language governing
-permissions and limitations under the License.
-************************************************************************************/
-
-using UnityEngine;
-using System.Collections;
-
-#if UNITY_ANDROID && !UNITY_EDITOR
-
-public abstract class OVROnCompleteListener : AndroidJavaProxy
-{
-	public OVROnCompleteListener() : base("com.oculus.svclib.OnCompleteListener")
-	{
-	}
-
-	public abstract void onSuccess();
-
-	public abstract void onFailure();
-}
-
-#endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:311543cab9a0c7bf7b86b3b855b29eee9c0d314774ceb7fe9e014db1887b1c17
+size 1037
