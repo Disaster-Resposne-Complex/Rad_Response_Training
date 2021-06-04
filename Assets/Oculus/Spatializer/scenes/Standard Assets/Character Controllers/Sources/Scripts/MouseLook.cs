7153469f74c4e663ee54eca92b6d59eb20732c27
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-06-04 17:13:30 -0600

    More avatar stuff
    Replaced 1st person character capsule with UMA avatar. Updated camera script so he doesn't tip forward when looking down. Added a little mirror for kicks and giggles.

--- Assets/Oculus/Spatializer/scenes/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Oculus/Spatializer/scenes/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c1422c2d2159b873efb224c8dbb5c8f85f047472bab06372955158b3c66eaa9
-size 2070
+oid sha256:0e9a94b160bc66cb021ede971de2530625dd5f35a15a7383a9903f4f16c91cfd
+size 2303
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e76193b0ef3f6d4bba1e704fbd6ef963ea3603c49e7fc87922f082a83d3946a
-size 743
+oid sha256:1f3d7b216c2b7f6538b39a0521d19fd0d450b93a2add9714b8ccdd3709bca568
+size 833
