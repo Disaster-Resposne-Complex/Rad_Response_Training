2cea1ea7eaec12226677b52088b9fdcf30361929
MercJones <jackdunk@hotmail.com>
2021-06-21 12:32:28 -0600

    changed particle spawned rad sources to only track detectors in effective range

--- Assets/Scripts/ParticleCollider.cs
+++ Assets/Scripts/ParticleCollider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13525bb65baae1b8b0788ba5b71c253e3c8ee75bf27790cef079e6b60eaaf51f
-size 1192
+oid sha256:7d1f18cc1af3d5a9d9805da17486aff48b7b43d209e36fc757cd01ccba0df809
+size 1122
