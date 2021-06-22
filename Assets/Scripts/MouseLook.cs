9d5d43730d17ae7ff1a3d736cc9b1f481899032d
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-06-22 12:10:35 -0600

    Mouselook helps
    The UI elements can't be clicked in Cursor lockmode. So I added an if-statement to unlock the cursor when you hit escape, and re-locks when you double click anywhere on the screen. The camera also won't rotate unless the cursor is locked

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:784d957f2780e906f164f5c628799a9a09d3fbd6bb3e218d4bf7640189466a49
+size 3168
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb498183ab18a8e211b896b9dea4cf4d6fd616ff49b979d84326217e881dfa13
-size 935
+oid sha256:dc3156ffd9831908821cf1074ea2dfad68e7815d74f9a6223b87d8bba0994f21
+size 1089
