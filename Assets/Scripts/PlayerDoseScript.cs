06339b5bbd0e62e80478a3909080f232ac8346ce
MercJones <jackdunk@hotmail.com>
2021-06-24 09:45:28 -0600

    added visual representation to bomb. added background to dose readings

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:513fe4e7d5455932be4ac8c9cd757c9e052307327b67dd82f52a7ae4d642a64e
-size 1403
+oid sha256:03ceb3197086c74af85e1256d3e8432db1b95337f57555c8e14ccd01c22453bf
+size 1541
