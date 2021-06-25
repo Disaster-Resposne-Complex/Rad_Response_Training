302d5a31e1b213ecbd5621e585b8a895ee752be9
MercJones <jackdunk@hotmail.com>
2021-06-24 20:54:55 -0600

    readjusted collision response to more accurately disperse particles

--- Assets/Scripts/ParticleCollider.cs
+++ Assets/Scripts/ParticleCollider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff9422854612a6357b016b374f46860c7c3cc9214fd4490c43da2572ae99341b
-size 1324
+oid sha256:79caccf2719677a0b1e64b32b44911c219fca2fb33c0be676b2441eff2f5624b
+size 1453
