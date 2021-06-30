ef224357242325056339a1f7bee2576655ad3958
MercJones <jackdunk@hotmail.com>
2021-06-29 21:20:18 -0600

    setup button to detonate bomb at a random location and fixed gravity

---
+++ Assets/Scripts/Detonation.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50e584ed082da089c54319311093fa301cf25d25ab32a960059a912f2d28d635
+size 814
--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33fc77d26165aead5ff721167a976aea62b55c48286baf3c7122a39560dff14f
-size 1177
+oid sha256:83ccf4461f1bf78cb893836ce1d35e2bcca5e62865448c5dd2af3e9f5788e438
+size 1085
