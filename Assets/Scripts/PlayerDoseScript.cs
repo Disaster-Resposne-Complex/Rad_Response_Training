481ee0dcc0e6659687fce2fc91e91112d2f41159
MercJones <35279730+MercJones@users.noreply.github.com>
2021-07-02 11:06:19 -0600

    Normalized background
    adjusted backround dose rate to have a normalized distribution

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c7165cd314a516220f0d4ceaf19344a7c42456746566c54b9a17611d57ddc7d
-size 2106
+oid sha256:f96f521f9e75a1d8cf1adc1789806a5145b0f41f8f533fc42f7737c407e5b663
+size 2251
