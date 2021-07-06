b75cb03f49156f838acaf8053c619f8c440b95a2
MercJones <jackdunk@hotmail.com>
2021-07-06 10:44:38 -0600

    adjusted background normalization. set conversion between mrem and urem

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c7165cd314a516220f0d4ceaf19344a7c42456746566c54b9a17611d57ddc7d
-size 2106
+oid sha256:9e02bdfa67008fb9815ee35007ec6e81511f587010f54a437abed724465a4e6b
+size 2627
