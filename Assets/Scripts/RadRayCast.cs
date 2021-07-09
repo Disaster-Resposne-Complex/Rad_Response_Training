404d7e5b5e61b061c37ebd2f64e0ef9b60a30972
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-07-09 09:24:05 -0600

    Revert "further improved terrain. Made a check source for meters"
    This reverts commit 3ff604ea1ba69c3990e6f0310c36ab76a0354f78.

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f037452425eae00049b432c88182d7f296a9f391f1d8a59fea2139839ab8633
-size 2757
+oid sha256:44cfb0d27e7c0688e177fb984cc3afd8c703c9335c07256cff67c110d24e82ae
+size 2873
