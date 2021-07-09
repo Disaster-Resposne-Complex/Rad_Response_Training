4378869fdda7e640ebe6435072ef22dd19254196
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-07-09 09:17:04 -0600

    Forced commit

---
+++ Assets/SteamVR/Editor/SteamVR_AutoEnableVR.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9816112b7bf10ddd14fa12a8634a957723092ed5e69f4ab7217e96c6f96c41b
+size 16560
---
+++ Assets/SteamVR/OpenVRUnityXRPackage/Editor/OpenVRPackageInstaller.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8fda0bdb6714b985de845f0b558012cffdbd3521063c8144d157ed80394ec5c1
+size 20251
