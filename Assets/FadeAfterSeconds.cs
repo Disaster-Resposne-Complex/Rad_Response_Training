16635fa7eadef601e67950162f2bd27b1c941fa9
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-07-09 11:57:58 -0600

    Fade script adjustments

--- Assets/FadeAfterSeconds.cs
+++ Assets/FadeAfterSeconds.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c9f55ea6aac304dfa5b0f30e00fe9c0c03a9c47cf5fd2cdc1ff1d067ad6d5db
-size 1839
+oid sha256:5eb2cef8e861f6fbe961d5273c057b4b3db0f898b799a3d800c02cc2e058bdec
+size 1983
