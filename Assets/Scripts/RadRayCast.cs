36c70ff44e222380067e29c2ffd72ff7e2483ba6
MercJones <jackdunk@hotmail.com>
2021-07-14 11:54:59 -0600

    heightened bomb activity

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7e73c02063f2cd329a54c3575042ffee306c951f5fe2171fdf89e96620be7bc
-size 2983
+oid sha256:00197335c7a0bec5f86d78fe89edb3d33e989f5f7138ada88fca2dc77f7f0fd6
+size 2974
