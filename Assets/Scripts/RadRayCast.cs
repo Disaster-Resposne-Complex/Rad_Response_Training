e7d41407b2715595bbfeacbafefc2cc8ac8867c9
MercJones <jackdunk@hotmail.com>
2021-07-14 10:21:14 -0600

    adjusted visibility of smoke from bomb with a fireball effect

--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0efc3916e748b8ce3a323e4517384790249dd12bc397dfbddc4bd657e8c80105
+oid sha256:e7e73c02063f2cd329a54c3575042ffee306c951f5fe2171fdf89e96620be7bc
 size 2983
