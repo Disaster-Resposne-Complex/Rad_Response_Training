553a6823c3b70ec76034e36f5eb741e9e0028c5f
MercJones <jackdunk@hotmail.com>
2021-07-13 19:01:30 -0600

    setup second scenario to find a single source in a room

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa6b666c4d60bb246dcbf2cd6f0f09368f24d68e05aedd4e9ba28947bbe39b45
-size 3200
+oid sha256:b69e1d3cef95878a06a2389aea90fd8d1a3f88e0347c4e20b62a87352c971ae6
+size 3610
--- Assets/Scripts/RadRayCast.cs
+++ Assets/Scripts/RadRayCast.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6853e184cc93f2eddaf8ec1b471ee06527e9470cfef0b78856caf55d0bfc3ed4
-size 2973
+oid sha256:0efc3916e748b8ce3a323e4517384790249dd12bc397dfbddc4bd657e8c80105
+size 2983
