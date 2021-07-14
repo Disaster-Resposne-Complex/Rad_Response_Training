a70195a8cb5c625bab4f9b77f54b77abdefb845f
MercJones <jackdunk@hotmail.com>
2021-07-13 19:44:23 -0600

    adjusted background to more realistic levels (100 cpm, .5 ur/hr)

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b69e1d3cef95878a06a2389aea90fd8d1a3f88e0347c4e20b62a87352c971ae6
-size 3610
+oid sha256:4d33bc1e97abc95406483a3178a5018a60410281153be2df6b7881993790dc74
+size 3609
