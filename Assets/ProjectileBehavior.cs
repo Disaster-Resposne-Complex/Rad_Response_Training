8e02bb9ccde95ef9a52e2920c3283f9f770804ab
ZacharyFree <69356255+ZacharyFree@users.noreply.github.com>
2021-07-30 13:11:15 -0600

    Revert "Merge branch '2020UpgradeBranch' of https://github.com/Disaster-Resposne-Complex/Rad_Response_Training into 2020UpgradeBranch"
    This reverts commit b56b47564e4910d50d1af6dcd85cfb8e824e23c4, reversing
    changes made to cd89c6abf7af677e5cf9905eb99106610dca2a5f.

--- Assets/ProjectileBehavior.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e401a54af281d7bdccd6e4a582e32d4f7f08ff934e6b10cac7dd5974c7fcf245
-size 530
--- Assets/shoot.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f68d129abbb786bb6090efe04597829aa1da6726408d916d371f4fcfbcafe372
-size 452
