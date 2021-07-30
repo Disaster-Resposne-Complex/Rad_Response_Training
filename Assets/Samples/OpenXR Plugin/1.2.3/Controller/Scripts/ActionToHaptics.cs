3b8d949f3aa6d9b396b94707abfeab258463dc0a
Nirajan Bhattarai <bhatnira@isu.edu>
2021-07-30 15:30:40 -0600

    Teleportation using terrain as teleporation area

---
+++ Assets/Samples/OpenXR Plugin/1.2.3/Controller/Scripts/ActionToHaptics.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e61f87f0d02ba89d025071826882ae4bccd9f95cba3ab8c12f57b7ef921a3fa1
+size 807
--- Assets/SteamVR/InteractionSystem/Core/Scripts/Interactable.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/Interactable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0445def06c84b11582b183df81e449c317bf501f535959dd1325439d8908458
-size 14904
+oid sha256:4cc77ec0d223c948ef6bd796cb3faab53744bdfbeaa466ec92d70f596023fe09
+size 14823
