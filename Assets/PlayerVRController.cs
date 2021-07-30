00bf37ff6f0e2b0366ffe5a7e3a4c0c84f73a811
MercJones <35279730+MercJones@users.noreply.github.com>
2021-07-30 14:07:34 -0600

    redid acceleration method

--- Assets/PlayerVRController.cs
+++ Assets/PlayerVRController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7021d72e4d062d237a8881e42717b8f21d6136d208c07cd08bec45afe77489f4
-size 765
+oid sha256:1408330527fbdd8c0efde08aac8d80a713e6575eb220a9957e2cdc7c5e5d36c1
+size 1261
