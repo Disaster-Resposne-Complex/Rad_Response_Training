cd89c6abf7af677e5cf9905eb99106610dca2a5f
MercJones <35279730+MercJones@users.noreply.github.com>
2021-07-30 11:28:02 -0600

    Accelerated movement
    Setup accelerated movement. No deceleration yet
    fixed bomb emission pattern

--- Assets/PlayerVRController.cs
+++ Assets/PlayerVRController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7021d72e4d062d237a8881e42717b8f21d6136d208c07cd08bec45afe77489f4
-size 765
+oid sha256:394db3b66e20d35d47880b9cd3e6a4df1233aead149484df279ccaf3572feb29
+size 1427
