2019f1e512c32bdac5790c21264dcafc8db12e36
MercJones <jackdunk@hotmail.com>
2021-08-22 17:18:26 -0600

    added an attach zone to the player

--- Assets/Scripts/why did i make these/AttachScript.cs
+++ Assets/Scripts/why did i make these/AttachScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:007320c0237129787ee12f828b64bcf3e3d50af9ece6b5952672e6b461cd9322
-size 1293
+oid sha256:6e828a5b3cd70712005ba2bf69f121a7deb1abe98e926c57662ad3ab5247efcf
+size 1594
