f47c1fe20839d7e1d30c8fe457d6bfef297e904e
MercJones <35279730+MercJones@users.noreply.github.com>
2021-08-26 13:32:00 -0600

    whole new bomb dispersal pattern
    added changing random wind and lower gravity to particles

---
+++ Assets/Scripts/WindRanomizer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:079a1042883909ce33e6e22805710d73555b4d502ae91109f12be0d97358322b
+size 390
--- Assets/Scripts/why did i make these/AttachScript.cs
+++ Assets/Scripts/why did i make these/AttachScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e828a5b3cd70712005ba2bf69f121a7deb1abe98e926c57662ad3ab5247efcf
-size 1594
+oid sha256:2680e9fbc64fd497ae26a0dc28680e0a935553c2ec0f120dd17d9b6345fd3d02
+size 1598
