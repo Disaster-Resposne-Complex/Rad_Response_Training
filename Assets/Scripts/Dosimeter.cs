3b3e84ff604a436b3f0971b2c6cb86f87b7d5e71
MercJones <35279730+MercJones@users.noreply.github.com>
2021-10-29 10:39:53 -0600

    Fixed dosimeter sensitivity

--- Assets/Scripts/Dosimeter.cs
+++ Assets/Scripts/Dosimeter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13af9e1a984d8bd6061ef5030498935c036caf7c24b0c3f9e43cde1944bd9c8e
-size 864
+oid sha256:10f23cd18c0a76e786262f22026b92cefb68dae177f54c3ecdabbd0ac21b2a18
+size 866
