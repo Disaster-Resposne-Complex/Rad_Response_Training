3bb4ff82dcb807c0e2032e6a17c7c7b05cf663ef
MercJones <35279730+MercJones@users.noreply.github.com>
2021-12-03 13:32:49 -0700

    Added more accurate GPS item. Merged to branch

--- Assets/GPSScript.cs
+++ Assets/GPSScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25ed67c210d9cbca9add5f8da700d522e3bbafe7ab5d7c94b983c7c0c09567fc
+oid sha256:a447a12692839cd9d4dbb42e9babe59d2eb33ac337ba6e473812606befe97cb4
 size 553
