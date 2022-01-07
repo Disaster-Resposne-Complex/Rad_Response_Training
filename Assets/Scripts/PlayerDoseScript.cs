b289832f96bffc8e07f5b7bc7df123fe6f3fa883
MercJones <35279730+MercJones@users.noreply.github.com>
2022-01-07 15:05:42 -0700

    Adjusted background radiation and meter response

--- Assets/Scripts/PlayerDoseScript.cs
+++ Assets/Scripts/PlayerDoseScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d33bc1e97abc95406483a3178a5018a60410281153be2df6b7881993790dc74
-size 3609
+oid sha256:13b953d8a8883edc6de6159517ac64a9ba5c12e6e1ef17d2b3221f96ba2c6038
+size 3608
