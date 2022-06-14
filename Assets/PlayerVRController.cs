1022fc535c28f21c252ca4457db119fae4a2f7f4
MercJones <35279730+MercJones@users.noreply.github.com>
2022-06-14 15:50:02 -0600

    fixed mouse input dropping in built version

--- Assets/PlayerVRController.cs
+++ Assets/PlayerVRController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:017da8e77dad000f287077a7282bc78b5db674afe8dbebc69a708b73257b6dc9
-size 1267
+oid sha256:186575ceea1ba0999a944544322821302a72edd03b201a174a92cec6edf02558
+size 1320
